34b7a4c4ee22dbd352632ceb1e4b4f6a40a44617
Satwik Kambham <satwik.kambham@gmail.com>
2024-10-20 08:41:17 +0530

    Added core module boilerplate files

---
+++ Source/UnrealQuickStartCore/UnrealQuickStartCore.Build.cs
@@ -0,0 +1,13 @@
+using UnrealBuildTool;
+
+public class UnrealQuickStartCore : ModuleRules
+{
+	public UnrealQuickStartCore(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+	}
+}
