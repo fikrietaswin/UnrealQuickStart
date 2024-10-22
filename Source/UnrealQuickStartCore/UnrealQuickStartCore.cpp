e48048ff78c32b3c16d43718b44c9689d8a0e0d5
Fikri Taswin <fikrietaswin@gmail.com>
2024-10-22 13:47:39 +0000

    optimizing a little

--- Source/UnrealQuickStartCore/UnrealQuickStartCore.cpp
+++ Source/UnrealQuickStartCore/UnrealQuickStartCore.cpp
@@ -1,6 +1,25 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-#include "UnrealQuickStartCore.h"
+#include "ARQuickStartCore.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealQuickStartCore, "UnrealQuickStartCore" );
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, ARQuickStartCore, "ARQuickStartCore");
+
+// Entry point for the AR system
+void FARQuickStartCore::StartupModule()
+{
+    Super::StartupModule();
+    
+    // Initialize AR session
+    InitializeARSession();
+}
+
+void FARQuickStartCore::InitializeARSession()
+{
+    // Set up AR session configuration (e.g., ARCore or ARKit)
+    UARSessionConfig* ARConfig = NewObject<UARSessionConfig>();
+    ARConfig->SetSessionType(EARSessionType::World);
+    ARConfig->bUseSceneDepth = true;
+    
+    UARBlueprintLibrary::StartARSession(ARConfig);
+}
